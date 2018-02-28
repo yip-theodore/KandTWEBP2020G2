1a4ccc882b239e3961f84d7af8898b5da4dd77bc
bunkermaster <yann.le-scouarnec@hetic.net>
2018-02-28 12:31:00 +0100

    Facto + new page 😘

---
+++ tokyo.php
@@ -0,0 +1,10 @@
+<?php include "includes/header.php"?>
+<div class="container theme-showcase" role="main">
+    <div class="jumbotron">
+        <h1>Tokyo!</h1>
+        <p>Suggoi</p>
+        <span class="label label-success">Onegai shimasu</span>
+    </div>
+    <img class="img-thumbnail" alt="Iron Maiden" src="img/snorkies.jpg" data-holder-rendered="true">
+</div>
+<?php include "includes/footer.php"?>
