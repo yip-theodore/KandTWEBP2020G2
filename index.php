58843aac025a9e4ed2834031ed0abc0d4c6ef91e
bunkermaster <yann.le-scouarnec@hetic.net>
2018-03-07 14:08:17 +0100

    Factorisation content

--- index.php
+++ index.php
@@ -7,7 +7,23 @@ require_once "includes/contentArray.php";
 // gestion de la page par defaut (pas de param page= dans l'url
 // gestion de la page appelee (param page=)
 // gestion de l'affichage de la page par defaut si la page appelee n'existe pas (avec le status 404 http)
-$page = $content['el-miedo-de-los-teletubbies'];
+define('APP_DEFAULT_PAGE', 'el-miedo-de-los-teletubbies');
+// la page par defaut n'existe pas, horreur, malheur
+if (!isset($content[APP_DEFAULT_PAGE])) {
+    die('omfg');
+}
+$pageKey = $_GET['page'] ?? APP_DEFAULT_PAGE;
+if (!isset($content[$_GET['page']])) {
+    // recuperation de la page par defaut
+    $page = &$content[APP_DEFAULT_PAGE];
+} else {
+    // recuperation de la page demandee
+    $page = &$content[$pageKey] ?? null;
+}
+if(null === $page){
+    // renvoi du code http 404 si page demande inexistante
+    http_response_code(404);
+}
 ?>
     <div class="container theme-showcase" role="main">
         <div class="jumbotron">
