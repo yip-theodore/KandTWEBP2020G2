6c0e54baf9baa285fb80c8a6623386e85a64f58b
Theodore <theodoreyip19@gmail.com>
2018-03-07 13:01:06 +0100

    +

--- index.php
+++ index.php
@@ -7,7 +7,30 @@ require_once "includes/contentArray.php";
 // gestion de la page par defaut (pas de param page= dans l'url
 // gestion de la page appelee (param page=)
 // gestion de l'affichage de la page par defaut si la page appelee n'existe pas (avec le status 404 http)
-$page = $content['el-miedo-de-los-teletubbies'];
+//$page = $content['el-miedo-de-los-teletubbies'];
+
+/*
+if (isset($_GET['page'])) {
+    if (isset($content[$_GET['page']])) {
+        $page = &$content[$_GET['page']];
+    } else {
+        http_response_code(404);
+        $page = &$content['el-miedo-de-los-teletubbies'];
+    }
+} else {
+    $page = &$content['el-miedo-de-los-teletubbies'];
+} */
+
+$page = &$content[$_GET['page']] ?? &$content['el-miedo-de-los-teletubbies'];
+if (isset($_GET['page']) && !isset($content[$_GET['page']])) {
+    http_response_code(404);
+}
+
+
+
+
+
+
 ?>
     <div class="container theme-showcase" role="main">
         <div class="jumbotron">
@@ -20,3 +43,4 @@ $page = $content['el-miedo-de-los-teletubbies'];
 <?php
 include "includes/footer.php";
 ?>
+
