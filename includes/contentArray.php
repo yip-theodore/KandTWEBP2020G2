2d13b3039c5fa6a6380fcf5948bf6dfdab598e87
bunkermaster <yann.le-scouarnec@hetic.net>
2018-03-07 11:43:34 +0100

    Facto du contenu dans un tableau

---
+++ includes/contentArray.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @author Yann Le Scouarnec <bunkermaster@gmail.com>
+ * Date: 07/03/2018
+ * Time: 11:24
+ */
+$content = [
+    "el-miedo-de-los-teletubbies" => [
+        "title" => "Les Teletubbies",
+        "h1" => "Les Teletubbies",
+        "p" => "C'est flippant, mais flippant.",
+        "span-class" => "label-danger",
+        "span-text" => "Danger",
+        "img-alt" => "Teletubbies",
+        "img-src" => "teletubbies.jpg",
+    ],
+    "not-ac-dc" => [
+        "title" => "Iron Maiden!",
+        "h1" => "Iron Maiden!",
+        "p" => "C'est vieux.",
+        "span-class" => "label-success",
+        "span-text" => "SIX SIX SIX!!! the number of the beast!!",
+        "img-alt" => "Iron Maiden",
+        "img-src" => "ironmaiden.jpg",
+    ],
+    "" => [
+        "title" => "",
+        "h1" => "",
+        "p" => "",
+        "span-class" => "",
+        "span-text" => "",
+        "img-alt" => "",
+        "img-src" => "",
+    ],
+    "" => [
+        "title" => "",
+        "h1" => "",
+        "p" => "",
+        "span-class" => "",
+        "span-text" => "",
+        "img-alt" => "",
+        "img-src" => "",
+    ],
+];
--- index.php
+++ index.php
@@ -1,10 +1,22 @@
-<?php include "includes/header.php"?>
+<?php
+include "includes/header.php";
+require_once "includes/contentArray.php";
+/**
+ * @ref http://php.net/manual/fr/function.http-response-code.php
+ */
+// gestion de la page par defaut (pas de param page= dans l'url
+// gestion de la page appelee (param page=)
+// gestion de l'affichage de la page par defaut si la page appelee n'existe pas (avec le status 404 http)
+$page = $content['el-miedo-de-los-teletubbies'];
+?>
     <div class="container theme-showcase" role="main">
         <div class="jumbotron">
-            <h1>Les Teletubbies</h1>
-            <p>C'est flippant.</p>
-            <span class="label label-danger">Danger</span>
+            <h1><?=$page['h1']?></h1>
+            <p><?=$page['p']?></p>
+            <span class="label <?=$page['span-class']?>"><?=$page['span-text']?></span>
         </div>
-        <img class="img-thumbnail" alt="Teletubbies" src="img/teletubbies.jpg" data-holder-rendered="true">
+        <img class="img-thumbnail" alt="<?=$page['img-alt']?>" src="img/<?=$page['img-src']?>" data-holder-rendered="true">
     </div>
-<?php include "includes/footer.php"?>
+<?php
+include "includes/footer.php";
+?>
