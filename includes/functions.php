93c980e4b03fb4c64a2c720d6a398242d5141874
bunkermaster <yann.le-scouarnec@hetic.net>
2018-03-28 11:28:30 +0200

    💂🏻‍♂️ preared the field for surgery

--- includes/functions.php
+++ includes/functions.php
@@ -20,3 +20,23 @@ function li($page, $linkText)
     <li<?=$active?>><a href="<?=$page?>"><?=$linkText?></a></li>
 <?php
 }
+
+/**
+ * display one page
+ * @param array $page
+ */
+function displayPage(array $page) : void
+{
+?>
+    <div class="container theme-showcase" role="main">
+        <div class="jumbotron">
+            <h1><?=$page['h1']?></h1>
+            <p><?=$page['p']?></p>
+            <span class="label <?=$page['span-class']?>"><?=$page['span-text']?></span>
+        </div>
+        <img class="img-thumbnail" alt="<?=$page['img-alt']?>" src="img/<?=$page['img-src']?>" data-holder-rendered="true">
+    </div>
+<?php
+}
+
+
--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
+require_once "includes/connection.php";
 include "includes/header.php";
-require_once "includes/contentArray.php";
+//require_once "includes/contentArray.php";
 /**
  * @ref http://php.net/manual/fr/function.http-response-code.php
  */
 // gestion de la page par defaut (pas de param page= dans l'url
-// gestion de la page appelee (param page=)
-// gestion de l'affichage de la page par defaut si la page appelee n'existe pas (avec le status 404 http)
 define('APP_DEFAULT_PAGE', 'el-miedo-de-los-teletubbies');
 // la page par defaut n'existe pas, horreur, malheur
 if (!isset($content[APP_DEFAULT_PAGE])) {
     die('omfg');
 }
+// gestion de la page appelee (param page=)
 $pageKey = $_GET['page'] ?? APP_DEFAULT_PAGE;
 if (!isset($content[$_GET['page']])) {
     // recuperation de la page par defaut
@@ -20,19 +20,12 @@ if (!isset($content[$_GET['page']])) {
     // recuperation de la page demandee
     $page = &$content[$pageKey] ?? null;
 }
+// gestion de l'affichage de la page par defaut si la page appelee n'existe pas (avec le status 404 http)
 if(null === $page){
     // renvoi du code http 404 si page demande inexistante
     http_response_code(404);
+    $page = &$content[$pageKey] ?? null;
 }
-?>
-    <div class="container theme-showcase" role="main">
-        <div class="jumbotron">
-            <h1><?=$page['h1']?></h1>
-            <p><?=$page['p']?></p>
-            <span class="label <?=$page['span-class']?>"><?=$page['span-text']?></span>
-        </div>
-        <img class="img-thumbnail" alt="<?=$page['img-alt']?>" src="img/<?=$page['img-src']?>" data-holder-rendered="true">
-    </div>
-<?php
+// factorisied the content display
+displayPage($page);
 include "includes/footer.php";
-?>
